3a50b9dee43d2b8d0efb9a8c46b4f7265d659f4a
docmartin87 <doc.martin87@gmail.com>
2021-01-14 22:54:33 +0000

    nearly there

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const cards = document.querySelectorAll('.card');                       /* list of all card elements stored in cards */
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard (){                                                   /* function called on click */
-    console.log('I was clicked');
-    console.log(this);
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard){
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+    
+        // do cards match?
+        console.log(firstCard.dataset.cardId);
+        console.log(secondCard.dataset.cardId);
+    }
 }
+
 
 cards.forEach (card => card.addEventListener('click', flipCard));       /* loop through list and on click then fire flipcard*/
