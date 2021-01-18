a0d2245690c986225080c9083185c2aaced359a5
docmartin87 <doc.martin87@gmail.com>
2021-01-18 18:14:36 +0000

    card game working

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
-const cards = document.querySelectorAll('.card');                       /* list of all card elements stored in cards */
+const cards = document.querySelectorAll('.card');                /* list of all card elements stored in cards */
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard (){                                                   /* function called on click */
-    this.classList.toggle('flip');
+function flipCard() {                                                   /* function called on click */
+  if (lockBoard) return;
+  if(this === firstCard) return;
 
-    if (!hasFlippedCard){
-        // first click
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
-    
-        // do cards match?
-        console.log(firstCard.dataset.cardId);
-        console.log(secondCard.dataset.cardId);
-    }
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.cardid === secondCard.dataset.cardid;       /* iterinary operator */
 
-cards.forEach (card => card.addEventListener('click', flipCard));       /* loop through list and on click then fire flipcard*/
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){                                                /* immediately invoked function expression */
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 16);
+        card.style.order = randomPosition;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));       /* loop through list and on click then fire flipcard*/
